c2505abbc6b87153c53f7f8869752e6e77607b9f
agnibrata86 <agninba@gmail.com>
2019-12-31 20:26:43 -0800

    check for invalid input

--- start.py
+++ start.py
@@ -17,8 +17,8 @@ def main(players):
 
 if __name__ == '__main__':
     names = [name.strip() for name in input('Enter Names of Player1 and Player2 with comma separated: ').split(',')]
-    print(names)
     if len(names) == 2:
         main(names)
     else:
-        print('Invalid inputs. Usage example: <Name1>,<Name2>')
+        print('Only 2 players are allowed. You have entered: ', names,
+              '\nUsage: <Player1_Name>, <Player2_Name>')
